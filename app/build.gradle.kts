89bfc4132b63583a66a89f6492a3b0c04e6f469a
LeTo <wellington87rodrigues@gmail.com>
2025-07-08 22:55:46 -0300

    Prática 06 – Parte 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.androidx.tools.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
